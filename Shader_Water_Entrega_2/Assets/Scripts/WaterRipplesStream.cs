fea8677f91e9edcf6ce1dba1f3982e76e62003c0
Segoo <68073260+Michikatsu0@users.noreply.github.com>
2023-04-09 21:27:24 -0500

    Update

--- Shader_Water_Entrega_2/Assets/Scripts/WaterRipplesStream.cs
+++ Shader_Water_Entrega_2/Assets/Scripts/WaterRipplesStream.cs
@@ -7,8 +7,7 @@ public class WaterRipplesStream : MonoBehaviour
     [SerializeField] private Material material;
     [SerializeField] private float radius;
 
-    private void Update()
-    {
+    private void Update()    {
         if (material == null) return;
         Vector3 p = transform.position;
         material.SetVector("_RipplesCenter",new Vector4(p.x,p.y,p.z, radius));
